ef4350bdb8af3b947ae6f774252ae1d0cf4c65fc
SarahIT200 <Sarah.Ali.abt.js@tuwaiq.edu.sa>
2021-10-12 20:19:56 +0300

    hw3 commit

---
+++ hw3.js
@@ -0,0 +1,16 @@
+//Ex1
+let radius = 15
+let circumference = 2 * 3.14 * radius
+let area = radius * radius * 3.14
+console.log("The circumference is " + circumference + ".")
+console.log("The area is " + area + ".")
+
+
+//Ex2
+let celsius = 32
+let fahrenheit = (celsius * 9 / 5) + celsius
+console.log(celsius + "°C is " + fahrenheit + "°F.")
+celsius = (fahrenheit - 32) * 5 / 9
+console.log(fahrenheit + "°F is " + celsius + "°C.")
+
+
