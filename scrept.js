42142e4df54226230f18bf860232d437748dbf8e
Wa2999 <92243050+Wa2999@users.noreply.github.com>
2021-10-12 23:28:40 +0300

    homework3

---
+++ scrept.js
@@ -0,0 +1,2 @@
+let radius = 2
+console.log("The area is" + (2 * radius * 3.14))
---
+++ wj.js
@@ -0,0 +1,8 @@
+//Convert it to fahrenheit
+let celsiustemperature = 28 * 9 / 5 + 32
+console.log(28 + "C " + " is " + celsiustemperature + "F")
+
+//Convert it to celsius 
+
+let fahrenheittemperature = (5 / 9) * (120 - 32)
+console.log(120 + "F " + " is " + fahrenheittemperature + "C")
