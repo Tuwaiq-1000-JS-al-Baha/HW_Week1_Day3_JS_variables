a2a376fac56ba660e4e429690c5cb37ffdf04f46
Eyad5501 <Eyad.Ahmed.abt.js@tuwaiq.edu.sa>
2021-10-12 21:42:13 +0300

    Create indexc,f.js

---
+++ indexc,f.js
@@ -0,0 +1,7 @@
+let celsius  = prompt(" Convert Celsius to Fahrenheit Enter Celsius ");
+let Cfahrenheit=(celsius+32)*1.8;
+console.log(celsius+"°C is " +Cfahrenheit +"°F");
+
+let Fahrenheit = prompt(" Convert Fahrenheit to Celsius  Enter  Fahrenheit  ");
+let CCelsius=(Fahrenheit-32)/1.8;
+console.log(Fahrenheit+"°F is " +CCelsius +"°C");
