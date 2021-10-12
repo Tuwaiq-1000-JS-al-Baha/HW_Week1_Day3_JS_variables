17d80d8cc14b208bdf6978adafb01cd78fcb11d2
AmjadMua <92171759+AmjadMua@users.noreply.github.com>
2021-10-12 22:44:27 +0300

    Day3-HomeWork

---
+++ TemperatureConverter.js
@@ -0,0 +1,7 @@
+let celsiustemperature = 30
+let convertToF = celsiustemperature * 1.8 + 32
+console.log(celsiustemperature + "°C is " + convertToF + "°F")
+
+let fahrenheittemperature = 86
+let convertToC = (fahrenheittemperature - 32) / 1.8
+console.log(fahrenheittemperature + "°F is " + convertToC + "°C")
---
+++ circumference.js
@@ -0,0 +1,7 @@
+let radius = 5
+let circumference = radius * 3.14 * 2
+let Diameter = radius * radius
+let area = 3.14 * Diameter
+
+console.log("The circumference is " + circumference)
+console.log("The area is " + area)
