a8197c7614703e719095aaab82c92980327c6f7f
SuaadAlgamdi <Suaad.Abduallh.abt.js@tuwaiq.edu.sa>
2021-10-12 22:36:13 +0300

    this is mr hw 3

---
+++ my hw.js
@@ -0,0 +1,9 @@
+let radius = 30
+let circumference = 2 * 3.14 * radius
+let area = radius * radius * 3.14
+console.log("Store a" + radius + "into a variable." + " Calculate the " + circumference + " based on the " + radius + " , " + "and output The circumference is NN " + "Calculate the area based on the " + radius + "and output The" + circumference +"is NN."+" Calculate the "+ area +" based on the "+ radius +"," + " and output " + "The" + area +"isNN") 
+
+
+
+
+
