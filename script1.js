a70ffacfce3949e61b47e1727990342fb633a7a2
Najwaageel <Najwa.Ageel.abt.js@tuwaiq.edu.sa>
2021-10-12 19:33:44 +0300

    my homework 3

---
+++ script1.js
@@ -0,0 +1,9 @@
+let radius = 4
+let circumference = (radius * 2 * 3.14)
+
+console.log( "The circumference is : " + circumference)
+ 
+
+let area = ( 3.14 * (radius * radius))
+
+console.log( "The area is : " + area )
---
+++ script2.js
@@ -0,0 +1,14 @@
+let celsius = 26
+let fahrenheitTemperature = Number
+
+fahrenheitTemperature = (celsius * 1.18) + 32
+console.log(celsius + "C" + " is " + fahrenheitTemperature + "F")
+
+
+let fahrenheit = 40
+let celsiusTemperature = Number
+celsiusTemperature = (fahrenheit - 32) / 1.8
+console.log(fahrenheit + "F" + " is " + celsiusTemperature + "C")
+
+
+
