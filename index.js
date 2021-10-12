3e7e2c4f95a1a10e6beb354fe0cecb8936e87a82
Abeer5011 <92170094+Abeer5011@users.noreply.github.com>
2021-10-12 21:13:13 +0300

    HWday3

---
+++ index.js
@@ -0,0 +1,10 @@
+let radius = 3
+let circumference = 2
+let theEqualOf = 3 * 2
+console.log('The circumference is ' + theEqualOf + '.')
+
+
+let celsiusTemperature = '100°C'
+ let fahrenheit = '212°F'
+console.log(celsiusTemperature + ' is ' + fahrenheit)
+console.log(fahrenheit + ' is ' + celsiusTemperature)
