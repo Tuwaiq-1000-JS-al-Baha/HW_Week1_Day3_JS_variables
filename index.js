9214115080313c1a724f487065bcd80982de9329
Abeer5011 <92170094+Abeer5011@users.noreply.github.com>
2021-10-12 21:48:05 +0300

    fix the homework

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-let radius = 3
-let circumference = 2
-let theEqualOf = 3 * 2
-console.log('The circumference is ' + theEqualOf + '.')
+let radius = 4
+let circumference = 2 * 4
+let area = 3.14 * 2
 
+console.log('The circumference is ' + circumference + '.')
+console.log('The area is ' + area + '.')
 
 let celsiusTemperature = '100°C'
  let fahrenheit = '212°F'
+ 
 console.log(celsiusTemperature + ' is ' + fahrenheit)
 console.log(fahrenheit + ' is ' + celsiusTemperature)
