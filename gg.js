00946ac5ffe7af9946d678ca1aa7bf7bba503ed4
ReemAbdullah1420 <Reem.Abdullah.abt.js@tuwaiq.edu.sa>
2021-10-12 21:06:38 +0300

    reem

---
+++ gg.js
@@ -0,0 +1,18 @@
+let radius = 4
+let pa = 3.14
+console.log(typeof pa)
+console.log(typeof radius)
+let circumference = radius * pa * 2
+console.log(circumference)
+let area = radius * radius * pa
+console.log(area)
+console.log("The circumference is " + circumference)
+console.log("The area is " + area)
+let celsiusTemperature = 50
+let F = (celsiusTemperature * 1.8) + 32
+console.log("Cْ  is " + celsiusTemperature + " Fْ is " + F)
+
+
+let fahrenheit = 50
+let celsiusTemperature2 = (fahrenheit - 32) / 1.8
+console.log("Fْ is " + fahrenheit + " Cْ is " + celsiusTemperature2)
