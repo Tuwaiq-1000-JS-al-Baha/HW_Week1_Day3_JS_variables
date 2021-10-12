b4a919293526933c5250eb352343a53c9137d9b9
Meshoeternity <Mashael.othman.abt.js@tuwaiq.edu.sa>
2021-10-12 21:41:40 +0300

    Done hw

---
+++ hw.js
@@ -0,0 +1,7 @@
+let rad=30
+let circumference =(rad * 2 * 3.14)
+console.log( "The circumference is :" +circumference)
+
+let area=(3.14 * rad * rad)
+console.log(" The area is :" + area)
+
---
+++ hwtemp.js
@@ -0,0 +1,8 @@
+let temc =40
+let temf=(temc * 1.8 ) +32
+console.log(temc + " c is " + temf + " F  ")
+
+let temff= 104
+
+let temcc=(temff-32) /1.8
+console.log( temff + " F IS " + temcc + " C ")
