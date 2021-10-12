2e9e8cbb20086e08159a7700570a4fb45384cf03
lalama2000 <92187017+lalama2000@users.noreply.github.com>
2021-10-12 22:12:56 +0300

    Done

---
+++ Temperature.js
@@ -0,0 +1,8 @@
+let celsius = 25;
+let celsiusInF = (celsius*9)/5 + 32;
+console.log(celsius + '°C is ' + celsiusInF + '°F');
+let fahrenheit = 30;
+let fahrenheitInC = ((fahrenheit - 32)*5)/9;
+console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
+
+
---
+++ geoetrizer.js
@@ -0,0 +1,13 @@
+function calcCircumfrence(radius) {
+    var y = (Math.PI * radius) * 2;
+  console.log("The circumfrence is " + y + ".");
+  }
+  function calcArea(radius) {
+    var x = (Math.PI * radius) * radius;
+  console.log("The area is " + x + ".");
+  }
+  
+  calcCircumfrence(30);
+  calcArea(10);
+
+
