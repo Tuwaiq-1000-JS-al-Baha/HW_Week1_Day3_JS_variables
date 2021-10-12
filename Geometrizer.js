9dd45db1b7bfd5f52a0979da1b0347dc2bb45324
LatifahAlGhamdi <Latifah.Dafer.abt.js@tuwaiq.edu.sa>
2021-10-12 20:29:46 +0300

    Done HW3.

---
+++ Geometrizer.js
@@ -0,0 +1,6 @@
+let circleRadius = 15
+let circumferenceOfCircle = 2 * Math.PI * circleRadius;
+console.log("The circumference is " + circumferenceOfCircle)
+let area = Math.PI * (circleRadius * circleRadius)
+console.log("The area is " + area)
+
---
+++ TemperatureConverter.js
@@ -0,0 +1,6 @@
+let celsiusTemperature = 60
+let cToFahr = celsiusTemperature * 9 / 5 + 30
+console.log(celsiusTemperature + "°C is " + cToFahr + "°F")
+let fahrenheitTemperature = 45
+let fToCel = (fahrenheitTemperature - 32) * 5 / 9
+console.log(fahrenheitTemperature + "°F is " + fToCel + "°C.")
