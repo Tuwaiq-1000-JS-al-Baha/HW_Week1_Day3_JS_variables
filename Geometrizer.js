35f9c64bdb419d0142de83ddd672d2d585b45c69
Ghadah-Mohammed <92169172+Ghadah-Mohammed@users.noreply.github.com>
2021-10-12 23:52:53 +0300

    HW.day3

---
+++ Geometrizer.js
@@ -0,0 +1,6 @@
+let radius = 8
+let circumference = radius * 3.14 * 2
+let area = 3.14 * radius * radius
+console.log("The circumference" + circumference)
+console.log("the area is " + area)
+
---
+++ Temperature.js
@@ -0,0 +1,7 @@
+let celsius = 22
+let converterfahrenheit = celsius * 1.8 + 32
+console.log(celsius + "°C is " + converterfahrenheit + "°F")
+
+let fahrenheit = 71.6
+let convertercelsius = (fahrenheit - 32) / 1.8
+console.log(converterfahrenheit + " °F is " + convertercelsius + "°C")
