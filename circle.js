4bc8f62d5a9e2787fde17703a2573ebbf871eb73
rahafdm <rahaf.nasser.abt.js@tuwaiq.edu.sa>
2021-10-13 01:56:07 +0300

    done home work

---
+++ circle.js
@@ -0,0 +1,5 @@
+let radius = 5
+let circumference = radius * 3.14
+let area = 3.14 * radius**2
+console.log("The Circumference Is " + circumference)
+console.log("The Area Is " + area)
---
+++ temp.js
@@ -0,0 +1,7 @@
+let fNumber = 3
+let cNumber = 2
+let celsius = 5/9 * ( 32 - fNumber)
+let fahrenheit = cNumber * 1.8 + 32
+
+console.log( cNumber + "°C is " + fahrenheit + "°F")
+console.log( fNumber + "°F is " + celsius + "°C")
