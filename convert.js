65ff644d78638d18aff00bad82e18b2c0df5a250
RahmaJumaan55 <92191130+RahmaJumaan55@users.noreply.github.com>
2021-10-12 23:45:47 +0300

    done HW3

---
+++ convert.js
@@ -0,0 +1,7 @@
+let celsiustemperature = 30;
+let celsiusInF = (celsiustemperature * 9) / 5 + 32;
+console.log(celsiusTemperature + '°C is ' + celsiusInF + '°F');
+let fahrenheit = 20;
+let fahrenheitInC = ((fahrenheit - 32) * 5) / 9;
+console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
+
---
+++ script.js
@@ -0,0 +1,6 @@
+let circleRadius = 5
+let circumferenceOfCircle = 2 * Math.PI * circleRadius;
+console.log("The circumference is " + circumferenceOfCircle)
+let area = PI * (circleRadius * circleRadius)
+console.log("The area is " + area)
+
