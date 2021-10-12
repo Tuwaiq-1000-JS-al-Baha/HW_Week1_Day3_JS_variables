6db0c1b80113130d813f8684aec1245957b68ae1
Abdulathim96 <Abdulathim.Abdullah.abt.js@tuwaiq.edu.sa>
2021-10-12 20:33:01 +0300

    homeWork 3

---
+++ TheGeometrizer.js
@@ -0,0 +1,4 @@
+let radius = prompt('enter the radius')
+const b = 3.14
+const x = 2
+console.log('The area is ' + radius * x * b)
---
+++ TheTemperatureConverter.js
@@ -0,0 +1,9 @@
+let celsius = 20
+const a = 1.8
+const b = 32
+const c = celsius * a + b
+console.log('the temperature is ' + c + 'F')
+
+let fahrenheit = 70
+const d = (fahrenheit - b) / a
+console.log('the temperature is ' + d + 'C')
