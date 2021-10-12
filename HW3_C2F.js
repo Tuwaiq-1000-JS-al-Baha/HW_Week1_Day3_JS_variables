9077c02dfffad60a23907e2d285bfbf3bd7de74c
X702-2025 <mohmmad201048@gmail.com>
2021-10-12 17:47:42 +0300

    HW3_Done

---
+++ HW3_C2F.js
@@ -0,0 +1,6 @@
+let cTemp= (prompt("Enter a number for celsius Temperature"));
+let fahrenheitTemp= (cTemp * 9/5) +32
+console.log(cTemp+"°C is "+ fahrenheitTemp+"°F")
+let fTemp= (prompt("Enter a number for fahrenheit Temperature"));
+let celsiusTemp= (fTemp-32)*5/9
+console.log(fTemp+"°F is "+ celsiusTemp+"°C")
---
+++ HW3_CalculateArea.js
@@ -0,0 +1,5 @@
+let π=3.14
+let radius= prompt("Enter a number for radius:")
+let circumference= 2*π*radius
+let area= π*radius**2
+console.log("The circumference is "+ circumference +" and the area is "+ area)
