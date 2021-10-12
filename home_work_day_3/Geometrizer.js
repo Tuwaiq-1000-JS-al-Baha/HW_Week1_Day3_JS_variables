3f7dd07a654df22979aaa3fc2085e467b3d08851
mosab10010 <Mosab.Hasen.abt.js@tuwaiq.edu.sa>
2021-10-12 20:03:30 +0300

    my home work day 3

--- home_work_day_3/Geometrizer.js
+++ home_work_day_3/Geometrizer.js
@@ -1,5 +1,5 @@
-var radius = 10;
-var circumference = Math.PI * 2*radius;
+var radius = Number(prompt('enter a rediius'));
+var circumference = Math.PI * 2 * radius;
 console.log("The circumference is " + circumference);
-var area = Math.PI * radius*radius;
+var area = Math.PI * radius * radius;
 console.log("The area is " + area);
