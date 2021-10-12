2789a220c1a3312950a952c62a09423997aa331a
Arwa94a <Arwa.Abdullah.abt.js@tuwaiq.edu.sa>
2021-10-12 19:29:47 +0300

    done

---
+++ hw.js
@@ -0,0 +1,17 @@
+//The Geometrizer
+let radius = 3
+let pi = 3.14
+let circumference = c = (2 * (pi) * radius)
+console.log('The circumference is ', c)
+let area = A = pi * radius * radius
+console.log('The area is', A)
+//The Temperature Converter
+var celsius = "ce";
+ce = 33;
+var fahrenheit = "f";
+f = (ce * 1.8) + 32
+console.log(ce, "°C is", f, "°F");
+var f = 120
+var ce = (f - 32) * .5556
+console.log(f, "°F is", ce, "°C")
+
