ed64fd9321a4043bc6822f103d4ccd31f3caae59
salwa alqurashi <Salwa.Kheder.abt.js@tuwaiq.edu.sa>
2021-10-12 18:46:51 +0300

    H.W DAY 3

---
+++ TC.js
@@ -0,0 +1,6 @@
+let celsiusTemperature = 33
+let fahrenheit = (celsiusTemperature * 2 + 30)
+console.log("33°C is " + fahrenheit + " °F")
+let fahrenheit1 = 96
+let celsiusTemperature1 = ((fahrenheit1 - 30) / 2)
+console.log("96 °F is " + celsiusTemperature1 + " °C")
---
+++ g.js
@@ -0,0 +1,5 @@
+let radius = 3
+let circumferenc = (radius * 3.14)
+console.log("the circumferenc is  " + circumferenc)
+let area = (3.14 * (radius * radius))
+console.log("the area is " + area)
