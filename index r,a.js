7af3d901ba0f020c5b0f3ddc9270a318731f6892
Eyad5501 <Eyad.Ahmed.abt.js@tuwaiq.edu.sa>
2021-10-12 21:42:10 +0300

    Create index r,a.js

---
+++ index r,a.js
@@ -0,0 +1,5 @@
+let Cradius = prompt("enter radius")
+let circumference =2*3.14*Cradius;
+console.log("the circumferenc is the radius " + circumference);
+let area=3.14*Cradius*Cradius;
+console.log("the area" + area);
