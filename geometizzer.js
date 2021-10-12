0488e49d91b0703f87bcb39b3f538f756810fe20
Shahdgh <Shahad.Mohammed.abt.js@tuwaiq.edu.sa>
2021-10-13 00:54:48 +0300

    home work

---
+++ geometizzer.js
@@ -0,0 +1,7 @@
+///### The Geometrizer
+let radius = Number(prompt('ENTER REDIUS'));
+let circumference = Math.PI * 2 * radius
+console.log("The circumference is " + circumference);
+let area = (Math.PI* (radius* radius))
+console.log("the area is  " + area)
+
---
+++ temperatuer.js
@@ -0,0 +1,10 @@
+//## The Temperature Converter
+let celsius = 24
+let celsiusInf = (celsius * 9 )/ 5 + 32 
+console.log(celsius + ' °C is ' + celsiusInf + "F");
+
+let fahrenheit = 28
+let fahrenheitInc = ((fahrenheit - 32 )*5)/9;
+
+console.log( fahrenheit + " f is " + fahrenheitInc + " °C");
+
