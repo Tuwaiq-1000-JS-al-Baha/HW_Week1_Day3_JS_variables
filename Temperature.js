8882d3bb81bbc2c8d5a55bc5a7dff2fd37987b04
Mohammed-Dukhaykh <Mohammed.Obaidallah.abt.js@tuwaiq.edu.sa>
2021-10-12 21:27:53 +0300

    Done Homework

---
+++ Temperature.js
@@ -0,0 +1,6 @@
+let celsius = 19
+let  fahrenheit = 1.8 * celsius + 32
+console.log(`${celsius}°C is ${fahrenheit}°F`)
+
+let convCelsius = (fahrenheit - 32) / 1.8
+console.log(`${fahrenheit}°F is ${convCelsius}°C`) 
---
+++ TheGeometrizer.js
@@ -0,0 +1,6 @@
+let radius = 4
+let  circum = 2 * radius * 3.14 
+console.log('The circumference is ' + circum)
+
+let area = 3.14 * (radius * radius)
+console.log('The area is ' + area)
