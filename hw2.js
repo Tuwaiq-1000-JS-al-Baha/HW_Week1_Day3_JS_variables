0107707c749ed89cc63c8b681ba3ad9591a4590b
Najmah24 <92245799+Najmah24@users.noreply.github.com>
2021-10-13 08:52:42 +0300

    hw done

---
+++ hw2.js
@@ -0,0 +1,8 @@
+//convert it to fahrenheit 
+let celsiustemperature= 28 * 9 / 5 + 32
+console.log(28 + "c " + "is " + celsiustemperature + "F")
+
+//convert it to celsius 
+
+let fahrenheittemperature = (5 / 9) * (120 - 32)
+console.log(120 + "F" + "is" + fahrenheittemperature + "c")
