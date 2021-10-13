9e01c7560cc2e142cb0cc1e5f3d806a6d9eedc1b
bader0007 <89877474+bader0007@users.noreply.github.com>
2021-10-13 09:32:31 +0300

    Create hw3-1.js

---
+++ hw3-1.js
@@ -0,0 +1,6 @@
+let celsius = 25;
+let celsiusInf = (celsius*9)/5 + 32;
+console.log(celsius + "°C"+ celsiusInf + "°F");
+let fahrenheit = 60;
+let fahrenheitInc = ((fahrenheit -32)*5)/9;
+console.log(fahrenheit + "°F" is + fahrenheitInc + "°C");
