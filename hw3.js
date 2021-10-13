135aac95799f5071b12f0dcdfee3c11d5e580866
bader0007 <89877474+bader0007@users.noreply.github.com>
2021-10-13 09:32:27 +0300

    Create hw3.js

---
+++ hw3.js
@@ -0,0 +1,5 @@
+let radius = 10;
+let circumference = Math.PI * 2*radius;
+console.log("The circumference is " + circumference);
+let area = Math.PI * radius*radius;
+console.log("The area is "+ area);
