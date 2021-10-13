00d15c22135d68337e77c68372a66860db02cce5
SuaadAlgamdi <Suaad.Abduallh.abt.js@tuwaiq.edu.sa>
2021-10-13 07:07:23 +0300

    my addthis is my addition to the assignment

--- my hw.js
+++ my hw.js
@@ -4,6 +4,6 @@ let area = radius * radius * 3.14
 console.log("Store a" + radius + "into a variable." + " Calculate the " + circumference + " based on the " + radius + " , " + "and output The circumference is NN " + "Calculate the area based on the " + radius + "and output The" + circumference +"is NN."+" Calculate the "+ area +" based on the "+ radius +"," + " and output " + "The" + area +"isNN") 
 
 
-
+let celsius temperature = (10-32)/1.8 
 
 
